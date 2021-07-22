a918401a00d0616c1dd4c57a2d9533239e1b8cae
Alexandr-Kravchenko <sanya5478@gmail.com>
2021-07-22 22:56:10 +0300

    small fixes

--- task2/task2.js
+++ task2/task2.js
@@ -1,14 +1,5 @@
 const moment = require('moment');
 
-let employees = [
-    { name: 'Ваня Иванов', birthday: '2000-08-20' },
-    { name: 'Коля Новогодний', birthday: '2000-07-02' },
-    { name: 'Петя Петров', birthday: '2002-09-12' },
-    { name: 'Стас Рождественский', birthday: '2001-09-12' },
-    { name: 'Марина Майская', birthday: '2003-08-17' },
-    { name: 'Стас Неяснов', birthday: '2003-07-16' }
-]
-
 const months =[
    'Январь',
    'Февраль',
@@ -36,23 +27,21 @@ function formatMap(list, ) {
             employeeMap.set(birthMonth, [{name: user.name, birthday: birthDate}])
         }
     })
-
     return employeeMap;
 }
-
-
-//console.log(formatMap(employees))
 
 function formatStr(map) {
     let currentDate = new Date();
     let temp = map.get(currentDate.getMonth() + 1);
+    let result = [];
     for(let i = 0; i < temp.length; i += 1) {
         let birthday = temp[i].birthday;
-        let age = `${birthday.getFullYear()}0${birthday.getMonth()}${birthday.getDate()}`;
+        let age = `${birthday.getFullYear()}` + `0${birthday.getMonth() + 1}` + `${birthday.getDate()}`;
+        console.log(age)
         let formatedAge = moment(age).fromNow().match(/[0-9]+/g)
-        return `(${temp[i].birthday.getDate()}) - ${temp[i].name} (${formatedAge} года)`
+        result.push(`(${temp[i].birthday.getDate()}) - ${temp[i].name} (${formatedAge} лет)`); // плюаризация ждет)
     }
-         
+    return result
 }
 
-export default formatStr;
+export {formatMap, formatStr};
--- task2/task2.test.js
+++ task2/task2.test.js
@@ -1,9 +1,21 @@
-import formatStr from './task2';
+import {formatMap, formatStr} from './task2';
 
-let testMap = new Map();
-testMap.set(7, [{name: 'Test Testov', birthday: new Date('2002-07-12')}])
-describe('formatStr', () => {
-    it('something', () => {
-        expect(formatStr(testMap)).toBe(`(12) - Test Testov (19 года)`);
+let employees = [
+    { name: 'Ваня Иванов', birthday: '2000-08-20' },
+    { name: 'Коля Новогодний', birthday: '2000-07-02' },
+    { name: 'Петя Петров', birthday: '2002-09-12' },
+    { name: 'Стас Рождественский', birthday: '2001-09-12' },
+    { name: 'Марина Майская', birthday: '2003-08-17' },
+    { name: 'Стас Неяснов', birthday: '2003-07-16' }
+]
+describe('Sorted Employee\'s birthday list', () => {
+    it('should return map with key: day, value: {name, fullDate}', () => {
+        let tempMap = formatMap(employees)
+        expect(tempMap).toBe()
+    });
+    it('should return correctly formated string', () => {
+        let testMap = new Map();
+        testMap.set(7, [{name: 'Петя Петров', birthday: new Date('2002-09-12')}]);
+        expect(formatStr(testMap)).toEqual([`(12) - Петя Петров (18 лет)`]);
     });
 });
