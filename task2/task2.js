16b6bf0687f6fa561158e3593bcfe936f7daffad
Oleksandr Kravchenko <intern@intern-pc.interlink.office>
2021-07-23 15:54:34 +0300

    task2 works

--- task2/task2.js
+++ task2/task2.js
@@ -1,47 +1,88 @@
 const moment = require('moment');
+moment.locale('ru');
 
-const months =[
-   'Январь',
-   'Февраль',
-   'Март',
-   'Апрель',
-   'Май',
-   'Июнь',
-   'Июль',
-   'Август',
-   'Сентябрь',
-   'Ноябрь',
-   'Декабрь',
-];
+const allMonths = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Ноябрь', 'Декабрь'];
 
-function formatMap(list, ) {
+let employees = [
+    { name: 'Ваня Иванов', birthday: '2000-08-20' },
+    { name: 'Коля Новогодний', birthday: '2000-07-02' },
+    { name: 'Петя Петров', birthday: '2002-09-29' },
+    { name: 'Стас Рождественский', birthday: '2001-09-01' },
+    { name: 'Марина Майская', birthday: '2003-08-17' },
+    { name: 'Стас Неяснов', birthday: '2003-07-16' }
+]
+
+function formatMap(list) {
     let employeeMap = new Map();
     list.forEach((user) => {
         let birthDate = new Date(user.birthday);
         let birthMonth = birthDate.getMonth() + 1;
         if(employeeMap.has(birthMonth)) {
             let employees = employeeMap.get(birthMonth);
-            employees.push({name: user.name, birthday: birthDate})
+            employees.push(user);
             employeeMap.set(birthMonth, employees)
         } else {
-            employeeMap.set(birthMonth, [{name: user.name, birthday: birthDate}])
+            employeeMap.set(birthMonth, [user])
         }
     })
     return employeeMap;
 }
 
-function formatStr(map) {
-    let currentDate = new Date();
-    let temp = map.get(currentDate.getMonth() + 1);
+function formatStr(map, month) {
+    let employees = map.get(month);
     let result = [];
-    for(let i = 0; i < temp.length; i += 1) {
-        let birthday = temp[i].birthday;
-        let age = `${birthday.getFullYear()}` + `0${birthday.getMonth() + 1}` + `${birthday.getDate()}`;
-        console.log(age)
-        let formatedAge = moment(age).fromNow().match(/[0-9]+/g)
-        result.push(`(${temp[i].birthday.getDate()}) - ${temp[i].name} (${formatedAge} лет)`); // плюаризация ждет)
+    for(let i = 0; i < employees.length; i += 1) {
+        let birthday = employees[i].birthday;
+        let age = birthday.replace(/[-]+/g, '');
+        let formatedAge = moment(age).fromNow().match(/[0-9]+ (лет|года|год)/g)
+        result.push(`(${new Date(birthday).getDate()}) - ${employees[i].name} (${formatedAge})`); 
     }
-    return result
+    sortByDate(result)
+    return result.join('\n');
 }
 
-export {formatMap, formatStr};
+function sortByDate(employees) {
+    employees.sort((current, next) => {
+        return Number(current.match(/(?<=\()[0-9]+(?=\))/g)) > Number(next.match(/(?<=\()[0-9]+(?=\))/g)) ? 1 : -1
+    });
+}
+
+function sortByMonth(employees) {
+    employees.sort((current, next) => {
+        let idCurrentMonth = allMonths.indexOf(current.match(/^[А-Яа-я]+(?=\s)/g)[0]);
+        let idNextMonth = allMonths.indexOf(next.match(/^[А-Яа-я]+(?=\s)/g)[0]);
+        return idCurrentMonth > idNextMonth ? 1 : -1
+    });
+}
+
+function formatMonth(map, plan = 1) {
+    let result = [];
+    for(const [key, value] of map) {
+        let today = new Date();
+        if(plan === 0) {
+            if(today.getMonth() + 1 === key) {
+                result.push(formatList(map, key, today));
+            }
+        } else if (plan === 1) {
+            if(today.getMonth() + 1 === key || today.getMonth() + 2 === key) {
+                result.push(formatList(map, key, today));
+            }
+        } else if (plan === 2) {
+            if(today.getMonth() + 1 === key || today.getMonth() + 2 === key || today.getMonth() + 3 === key) {
+                result.push(formatList(map, key, today));
+            }
+        }
+    }
+    sortByMonth(result)
+    return result.join('\n');
+}
+
+function formatList(map, id, date) {
+    let result;
+    result = `${allMonths[id - 1]} ${date.getFullYear()}\n${formatStr(map, id)}`
+    return result;  
+}
+
+export {formatMap, formatStr, formatMonth, formatList, sortByMonth, sortByDate};
+
+//console.log(formatMonth(formatMap(employees)))
--- task2/task2.test.js
+++ task2/task2.test.js
@@ -1,21 +1,30 @@
-import {formatMap, formatStr} from './task2';
+import {formatMap, formatStr, formatMonth} from './task2';
 
 let employees = [
     { name: 'Ваня Иванов', birthday: '2000-08-20' },
     { name: 'Коля Новогодний', birthday: '2000-07-02' },
-    { name: 'Петя Петров', birthday: '2002-09-12' },
-    { name: 'Стас Рождественский', birthday: '2001-09-12' },
+    { name: 'Петя Петров', birthday: '2002-09-29' },
+    { name: 'Стас Рождественский', birthday: '2001-09-01' },
     { name: 'Марина Майская', birthday: '2003-08-17' },
     { name: 'Стас Неяснов', birthday: '2003-07-16' }
 ]
 describe('Sorted Employee\'s birthday list', () => {
     it('should return map with key: day, value: {name, fullDate}', () => {
-        let tempMap = formatMap(employees)
-        expect(tempMap).toBe()
+        let expectedMap = new Map().set(7, [
+            { name: 'Коля Новогодний', birthday: '2000-07-02' },
+            { name: 'Стас Неяснов', birthday: '2003-07-16' }
+        ]);
+        let receivedMap = formatMap(employees).get(7);
+        expect(receivedMap).toEqual(expectedMap.get(7))
     });
+
     it('should return correctly formated string', () => {
-        let testMap = new Map();
-        testMap.set(7, [{name: 'Петя Петров', birthday: new Date('2002-09-12')}]);
-        expect(formatStr(testMap)).toEqual([`(12) - Петя Петров (18 лет)`]);
+        let testMap = formatMap(employees);
+        testMap.get(7);
+        expect(formatStr(testMap)).toEqual([`(2) - Коля Новогодний (21 год)`, `(16) - Стас Неяснов (18 лет)`]);
     });
+
+/*     it('should count an age of employee', () => {
+        expect()
+    }); */
 });
